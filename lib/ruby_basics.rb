b62e8b983321c0587e27d2418bc5e26fd7f023df
Sarah Jacob <smjacob91@gmail.com>
2018-07-01 12:26:50 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,16 +1,17 @@
 def division(num1, num2)
- return num1/num2
+  num1/num2
 end
 
-def assign_variable(name=value)
-  return name
+def assign_variable(value)
+  value = "Bob"
 end
 
 def argue(phrase)
-  return phrase
+  phrase = "I'm right and you are wrong!"
 end
 
 def greeting(greeting,name)
+  
 end
 
 def return_a_value(phrase= "Nice")
